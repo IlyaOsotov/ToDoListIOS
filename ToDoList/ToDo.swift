869bcd7df0d05a7884d82d717699b25237528728
IlyaOsotov <soten58@yandex.ru>
2021-11-18 20:24:11 +0700

    init

---
+++ ToDoList/ToDo.swift
@@ -0,0 +1,32 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Илья Осотов on 16.11.2021.
+//
+
+import Foundation
+
+struct ToDo: Equatable {
+    let id = UUID()
+    var title: String
+    var isComplete: Bool
+    var dueDate: Date
+    var notes: String?
+    
+    static func ==(lhs: ToDo, rhs: ToDo) -> Bool {
+        return lhs.id == rhs.id
+    }
+    
+    static func loadToDos() -> [ToDo]? {
+        return nil
+    }
+    
+    static func loadSampleToDos() -> [ToDo] {
+        let todo1 = ToDo(title: "One", isComplete: false, dueDate: Date(), notes: "Note 1")
+        let todo2 = ToDo(title: "Two", isComplete: false, dueDate: Date(), notes: "Note 2")
+        let todo3 = ToDo(title: "Three", isComplete: false, dueDate: Date(), notes: "Note 3")
+        
+        return [todo1, todo2, todo3]
+    }
+}
